8c6794e4928150014d39c95266bba73bf05bb18b
Mingxin Zhang <m.zhang@hapis.u-tokyo.ac.jp>
2023-04-12 14:50:05 +0900

    Add SLS decoder

---
+++ Autoencoder.py
@@ -0,0 +1,125 @@
+'''
+Author: Mingxin Zhang m.zhang@hapis.u-tokyo.ac.jp
+Date: 2023-04-12 01:41:18
+LastEditors: Mingxin Zhang
+LastEditTime: 2023-04-12 14:47:07
+Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
+'''
+
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class Encoder(nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        
+        ### Convolutional section
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, 5, padding=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(2, return_indices=True)
+        self.conv3 = nn.Conv2d(16, 32, 5, padding=0)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d(2, return_indices=True)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.fc1 = nn.Linear(32 * 1 * 23, 128)
+        self.fc2 = nn.Linear(128, encoded_space_dim)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x, i_1 = self.pool1(self.conv2(x))
+        x = F.relu(self.bn1(x))
+        x, i_2 = self.pool2(self.conv3(x))
+        x = F.relu(self.bn2(x))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x, i_1, i_2
+
+
+class Decoder(nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(encoded_space_dim, 128)
+        self.fc2 = nn.Linear(128, 32 * 1 * 23)
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 1, 23))
+
+        self.deconv1 = nn.ConvTranspose2d(32, 16, 5, output_padding=0)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.unpool1 = nn.MaxUnpool2d(2)
+        self.deconv2 = nn.ConvTranspose2d(16, 8, 5, padding=2)
+        self.bn2 = nn.BatchNorm2d(8)
+        self.unpool2 = nn.MaxUnpool2d(2)
+        self.deconv3 = nn.ConvTranspose2d(8, 1, 3, padding=1)
+        
+    def forward(self, x, unpool_i1, unpool_i2):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.unflatten(x)
+        x = self.unpool1(x, unpool_i2)
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = self.unpool2(x, unpool_i1)
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = self.deconv3(x)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(encoded_space_dim, 16)
+        self.fc2 = nn.Linear(16, 7)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=1) #dim0=batch, dim1=element
+        return x
+    
+
+class AutoEncoder(nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        self.encoder = Encoder(encoded_space_dim)
+        self.decoder = Decoder(encoded_space_dim)
+        self.classifier = Classifier(encoded_space_dim)
+        
+    def forward(self, x):
+        feat, i_1, i_2 = self.encoder(x)
+        pred_img = self.decoder(feat, i_1, i_2)
+        pred_label = self.classifier(feat)
+        return feat, pred_img, pred_label
+    
+
+# Used for test without indices of pooling
+class TestDecoder(nn.Module):
+    def __init__(self, encoded_space_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(encoded_space_dim, 128)
+        self.fc2 = nn.Linear(128, 32 * 1 * 23)
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 1, 23))
+
+        self.deconv1 = nn.ConvTranspose2d(32, 16, 5, output_padding=0)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.deconv2 = nn.ConvTranspose2d(16, 8, 5, padding=2)
+        self.bn2 = nn.BatchNorm2d(8)
+        self.deconv3 = nn.ConvTranspose2d(8, 1, 3, padding=1)
+    
+    def upsample(self, A):
+        size = torch.tensor(A.shape)
+        size[-2:] *= 2
+        B = torch.zeros(list(size))
+        B[..., ::2, ::2] = A
+        return B.to("mps")
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.unflatten(x)
+        x = self.upsample(x)
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = self.upsample(x)
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = self.deconv3(x)
+        return x
---
+++ SLS_decoder.py
@@ -0,0 +1,105 @@
+'''
+Author: Mingxin Zhang m.zhang@hapis.u-tokyo.ac.jp
+Date: 2023-04-12 01:47:50
+LastEditors: Mingxin Zhang
+LastEditTime: 2023-04-12 14:47:30
+Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
+'''
+
+import pySequentialLineSearch
+import Autoencoder
+import torch
+import pickle
+import numpy as np
+import sys
+from torch import nn
+from typing import Optional, Tuple
+from sklearn.preprocessing import MinMaxScaler
+
+# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("mps")
+print(f'Selected device: {device}')
+
+# A dummy implementation of slider manipulation
+def ask_human_for_slider_manipulation(optimizer, decoder, norm_scaler, target_spec):
+    t_max = 0.0
+    f_max = -sys.float_info.max
+
+    mse = nn.MSELoss()
+
+    for i in range(1000):
+        
+        t = float(i) / 999.0
+
+        generated_vector = optimizer.calc_point_from_slider_position(t)
+
+        # denormalization
+        generated_vector = torch.unsqueeze(torch.tensor(generated_vector), 0)
+        generated_vector = torch.tensor(norm_scaler.inverse_transform(generated_vector)).to(torch.float32)
+        generated_vector = generated_vector.to(device)
+        decoded_spec = decoder(generated_vector)
+
+        f = -mse(decoded_spec, target_spec).item()
+
+        if f_max is None or f_max < f:
+            f_max = f
+            t_max = t
+
+    return t_max
+
+
+def main():
+    # Model initialization and parameter loading
+    # TestDecoder is used for test without indices of pooling
+    decoder = Autoencoder.TestDecoder(encoded_space_dim = 20)
+    # decoder_dict = torch.load('/content/drive/MyDrive/Colab Notebooks/vibrotactile-encoder/decoder.pt', map_location=torch.device('cpu'))
+    decoder_dict = torch.load('decoder.pt', map_location=torch.device('cpu'))
+    decoder_dict = {k: v for k, v in decoder_dict.items()}
+    decoder.load_state_dict(decoder_dict)
+
+    decoder.eval()
+    decoder.to(device)
+
+    # Load the extracted 20-dimensional features
+    with open('feat_dict.pickle', 'rb') as file:
+        feat_dict = pickle.load(file)
+
+    vib_feat = feat_dict['vib_feat']
+    original_vib = feat_dict['original_vib']
+
+    # Randomly choose a target spectrogram
+    target_index = np.random.randint(0, len(original_vib))
+    target_spec = original_vib[target_index]
+    target_spec = torch.unsqueeze(torch.tensor(target_spec), 0).to(device)
+
+    norm_scaler = MinMaxScaler()
+    norm_vib_feat = norm_scaler.fit_transform(vib_feat) # normalization
+
+    optimizer = pySequentialLineSearch.SequentialLineSearchOptimizer(
+        num_dims=20)
+
+    optimizer.set_hyperparams(kernel_signal_var=0.50,
+                              kernel_length_scale=0.10,
+                              kernel_hyperparams_prior_var=0.10)
+
+    for i in range(30):
+        # slider_ends = optimizer.get_slider_ends()
+        slider_position = ask_human_for_slider_manipulation(optimizer, decoder, norm_scaler, target_spec)
+        print(slider_position)
+        optimizer.submit_feedback_data(slider_position)
+
+        optimized_vector = optimizer.get_maximizer()
+        # denormalization
+        optimized_vector = torch.unsqueeze(torch.tensor(optimized_vector), 0)
+        optimized_vector = torch.tensor(norm_scaler.inverse_transform(optimized_vector)).to(torch.float32)
+        optimized_vector = optimized_vector.to(device)
+        decoded_spec = decoder(optimized_vector)
+
+        mse = nn.MSELoss()
+        loss = mse(decoded_spec, target_spec).item()
+        print("[#iters = " + str(i + 1) + "] loss: " + str(loss))
+
+
+if __name__ == '__main__':
+    main()
+
