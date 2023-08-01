60248f3ad22bd356c3503be9bd6800d7a146dc3a
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2023-08-01 13:43:25 +0900

    commit

--- DSS_UI.py
+++ DSS_UI.py
@@ -2,7 +2,7 @@
 Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
 Date: 2023-07-04 01:27:58
 LastEditors: Mingxin Zhang
-LastEditTime: 2023-07-29 15:17:52
+LastEditTime: 2023-08-01 13:43:00
 Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
 '''
 import sys
