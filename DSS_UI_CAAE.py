f3dc99a2e0c4ba064c0f928cb5516d4a5bf1a208
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2023-11-03 13:32:16 +0900

    commit

--- DSS_UI_CAAE.py
+++ DSS_UI_CAAE.py
@@ -2,13 +2,13 @@
 Author: Mingxin Zhang m.zhang@hapis.k.u-tokyo.ac.jp
 Date: 2023-07-04 01:27:58
 LastEditors: Mingxin Zhang
-LastEditTime: 2023-10-24 04:47:10
+LastEditTime: 2023-11-03 13:20:20
 Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
 '''
 import sys
 import numpy as np
 from GlobalOptimizer import JacobianOptimizer
-from CAAE_LMT108 import model
+from CAAE_14_norm import model
 import torch
 import pickle
 import sys
@@ -20,11 +20,11 @@ from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-device = torch.device("cpu")
+device = torch.device("mps")
 print(f'Selected device: {device}')
 
-FEAT_DIM = 256
-CLASS_NUM = 108
+FEAT_DIM = 128
+CLASS_NUM = 9
 
 def denormalize(img):
     # Min of original data: -80
@@ -42,7 +42,7 @@ def myFunc(decoder, zs):
     return output
 
 def myGoodness(target, xs):
-    xs = torch.tensor(xs).to(torch.float32).to(device)
+    xs = torch.tensor(xs).to(device)
     return np.sum((xs.reshape(xs.shape[0], -1) - target.reshape(1, -1)).cpu().detach().numpy() ** 2, axis=1) ** 0.5
 
 def myJacobian(model, z):
@@ -80,18 +80,18 @@ class HeatmapWindow(QMainWindow):
         self.setWindowTitle("Heatmap with Slider")
         self.setGeometry(100, 100, 400, 300)
 
-        model_name = 'CAAE_LMT108'
-        self.decoder = model.Generator(feat_dim=FEAT_DIM, class_dim=CLASS_NUM)
+        model_name = 'CAAE_14_norm'
+        self.decoder = model.Generator(feat_dim=FEAT_DIM)
 
         # Model initialization and parameter loading
-        decoder_dict = torch.load(model_name + '/generator_' + str(FEAT_DIM) + 'd_gamma10.pt', map_location=torch.device('cpu'))
+        decoder_dict = torch.load(model_name + '/generator_' + str(FEAT_DIM) + 'd.pt', map_location=torch.device('cpu'))
         decoder_dict = {k: v for k, v in decoder_dict.items()}
         self.decoder.load_state_dict(decoder_dict)
 
         self.decoder.eval() 
         self.decoder.to(device)
 
-        with open('trainset_LMT_large.pickle', 'rb') as file:
+        with open('trainset_7-class.pickle', 'rb') as file:
             trainset = pickle.load(file)
     
         index = np.random.randint(len(trainset['spectrogram']))
@@ -100,26 +100,23 @@ class HeatmapWindow(QMainWindow):
 
         target_data = torch.unsqueeze(torch.tensor(self.target_spec), 0).to(torch.float32).to(device)
 
-        slider_length = getSliderLength(FEAT_DIM+CLASS_NUM, 1, 0.2)
-        target_latent = np.random.uniform(-1, 1, FEAT_DIM+CLASS_NUM)
+        slider_length = getSliderLength(FEAT_DIM, 1, 0.2)
+        target_latent = np.random.uniform(-1, 1, FEAT_DIM)
         target_latent = torch.tensor(target_latent).to(torch.float32).to(device)
         # target_data = decoder(target_latent.reshape(1, -1))[0]
 
         while True:
-            random_A = getRandomAMatrix(FEAT_DIM+CLASS_NUM, 6, np.array(target_latent.reshape(1, -1).cpu()), 1)
+            random_A = getRandomAMatrix(FEAT_DIM, 6, np.array(target_latent.reshape(1, -1).cpu()), 1)
             if random_A is not None:
                 break
         # random_A = getRandomAMatrix(FEAT_DIM, 6, target_latent.reshape(1, -1), 1)
-        
-        # initialize the conditional part
-        # init_z_class = np.random.uniform(low=0, high=1, size=(CLASS_NUM))
-        init_z_class = np.zeros(CLASS_NUM)
-        init_z_noise = np.random.normal(loc=0.0, scale=1.0, size=(FEAT_DIM))
-        init_z = np.append(init_z_noise, init_z_class)
+
+        init_z = np.random.uniform(low=-2.5, high=2.5, size=(FEAT_DIM))
+        # init_z = np.random.normal(loc=0.0, scale=1.0, size=(FEAT_DIM))
         init_low_z = np.matmul(np.linalg.pinv(random_A), init_z.T).T
         init_z = np.matmul(random_A, init_low_z)
 
-        self.optimizer = JacobianOptimizer.JacobianOptimizer(FEAT_DIM+CLASS_NUM, 48*320, 
+        self.optimizer = JacobianOptimizer.JacobianOptimizer(FEAT_DIM, 48*320, 
                       lambda zs: myFunc(self.decoder, zs), 
                       lambda xs: myGoodness(target_data, xs), 
                       slider_length, 
