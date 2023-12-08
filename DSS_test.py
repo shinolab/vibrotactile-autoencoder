e328af395ceeebda7b84c85d6f2b1ab464886c72
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2023-12-08 17:45:46 +0900

    commit

--- DSS_test.py
+++ DSS_test.py
@@ -2,13 +2,12 @@
 Author: Mingxin Zhang m.zhang@hapis.u-tokyo.ac.jp
 Date: 2023-04-12 01:47:50
 LastEditors: Mingxin Zhang
-LastEditTime: 2023-10-10 02:50:12
+LastEditTime: 2023-12-08 17:44:40
 Copyright (c) 2023 by Mingxin Zhang, All Rights Reserved. 
 '''
 
-import pySequentialLineSearch
 from GlobalOptimizer import JacobianOptimizer
-from ResNet50_SRResNet_ACGAN_LMT108 import model
+from CAAE_14class import model
 import torch
 import pickle
 import numpy as np
@@ -19,13 +18,14 @@ from torchvision import transforms
 
 # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # device = torch.device("mps")
-device = torch.device("cpu")
+device = torch.device("cuda")
 print(f'Selected device: {device}')
 
-FEAT_DIM = 256
-CLASS_NUM = 108
+FEAT_DIM = 128
+CLASS_NUM = 14
+SLIDER_LEN = 30
 
-def denormalize(img):
+def img_denormalize(img):
     # Min of original data: -80
     # Max of original data: 0
     origin_max = 0.
@@ -34,9 +34,18 @@ def denormalize(img):
     denormalized_img = img * (origin_max - origin_min) + origin_min
     return denormalized_img
 
+def z_denormalize(z):
+    # range of real latent space: [-7.24, 6.42]
+    origin_max = 6.42
+    origin_min = -7.24
+    z = (z + 1) / 2
+    denormalized_z = z * (origin_max - origin_min) + origin_min
+    return denormalized_z
+
 def myFunc(decoder, zs):
+    zs = z_denormalize(zs)
     zs = torch.tensor(zs).to(torch.float32).to(device)
-    output = denormalize(decoder(zs)).reshape(zs.shape[0], -1)
+    output = img_denormalize(decoder(zs)).reshape(zs.shape[0], -1)
     # output = decoder(zs).reshape(zs.shape[0], -1)
     return output
 
@@ -45,8 +54,10 @@ def myGoodness(target, xs):
     return np.sum((xs.reshape(xs.shape[0], -1) - target.reshape(1, -1)).cpu().detach().numpy() ** 2, axis=1) ** 0.5
 
 def myJacobian(model, z):
+    z = z_denormalize(z)
     z = torch.tensor(z).to(torch.float32).to(device)
-    return model.calc_model_gradient(z, device)
+    output = model.calc_model_gradient(z, device)
+    return output
 
 def getSliderLength(n, boundary_range, ratio, sample_num=1000):
     samples = np.random.uniform(low=-boundary_range, high=boundary_range, size=(sample_num, 2, n))
@@ -72,9 +83,8 @@ def getRandomAMatrix(high_dim, dim, optimals, range):
         return None
 
 def main():
-    model_name = 'ResNet50_SRResNet_ACGAN_LMT108'
-    # model_name = 'ResNet50_SRResNet_ACGAN_7-class'
-    decoder = model.Generator(encoded_space_dim = FEAT_DIM)
+    model_name = 'CAAE_14class'
+    decoder = model.Generator(feat_dim=FEAT_DIM)
 
     result_dict = {'original': [], 
                   'generated': [], 
@@ -92,8 +102,8 @@ def main():
     # with open('sample_target_spec_1.pickle', 'rb') as file:
     #     target_spec = pickle.load(file)
 
-    with open('trainset_7-class.pickle', 'rb') as file:
-        trainset = pickle.load(file)
+    with open('testset_7-class.pickle', 'rb') as file:
+        testset = pickle.load(file)
 
     test_index = [68, 2248, 2947, 3893, 4181, 5437, 10451, 11387, 13158, 14095]
     
@@ -103,13 +113,13 @@ def main():
 
     # repeat generation experiments
     for index in test_index:
-        target_spec = trainset['spectrogram'][index]
-        soundfile = trainset['filename'][index]
+        target_spec = testset['spectrogram'][index]
+        soundfile = testset['filename'][index]
 
         target_data = torch.unsqueeze(torch.tensor(target_spec), 0).to(torch.float32).to(device)
 
-        slider_length = getSliderLength(FEAT_DIM, 1, 0.2)
-        target_latent = np.random.uniform(-1, 1, FEAT_DIM)
+        slider_length = getSliderLength(FEAT_DIM, 1, 0.8)
+        target_latent = np.random.uniform(-2.5, 2.5, FEAT_DIM)
         target_latent = torch.tensor(target_latent).to(torch.float32).to(device)
         # target_data = decoder(target_latent.reshape(1, -1))[0]
 
@@ -119,11 +129,7 @@ def main():
                 break
         # random_A = getRandomAMatrix(FEAT_DIM, 6, target_latent.reshape(1, -1), 1)
         
-        # initialize the conditional part
-        init_z_class = np.random.uniform(low=0, high=1, size=(CLASS_NUM))
-        # init_z_class = np.zeros(CLASS_NUM)
-        init_z_noise = np.random.normal(loc=0.0, scale=1.0, size=(FEAT_DIM - CLASS_NUM))
-        init_z = np.append(init_z_noise, init_z_class)
+        init_z = np.random.uniform(low=-2.5, high=2.5, size=(FEAT_DIM))
         init_low_z = np.matmul(np.linalg.pinv(random_A), init_z.T).T
         init_z = np.matmul(random_A, init_low_z)
 
@@ -142,7 +148,7 @@ def main():
         imdata = np.zeros(imshape)
         
         for i in range(iter_num):
-            n_sample = 1000
+            n_sample = SLIDER_LEN
             opt_z, opt_x, opt_score, opt_t = optimizer.find_optimal(n_sample, batch_size=n_sample)
             # print(opt_z)
             if opt_score < best_score:
